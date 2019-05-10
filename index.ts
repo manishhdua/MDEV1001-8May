313ef9344711365395db90574779c5641555228a
manishdua <mdua007@gmail.com>
2019-05-09 23:38:40 -0400

    Final

---
+++ index.ts
@@ -0,0 +1,13 @@
+class Printer
+{
+private printer : (str : string) => void;
+public constructor (func : (str: string)=>void)
+{
+    this.printer = func;
+}
+public print(str: string ):void{
+    this.printer(str);
+}
+}
+let printer = new Printer(console.log);
+printer.print("Hello world");
